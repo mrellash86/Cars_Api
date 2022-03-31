1bd90379bf4fa7824d696b5c9cdabcf8c031c13f
unknown <mrellash86@gmail.com>
2022-03-31 07:13:46 -0500

    Updated Modles py and settings py

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,9 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+class car(models.Model):
+    make = models.CharField(max_length=255)
+    model = models.CharField(max_length=255)
+    year = models.IntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
